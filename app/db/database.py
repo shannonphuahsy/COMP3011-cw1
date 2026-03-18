ae59cd6bfddc2e2ebe6405d4c4a09418a9e2c827
Shannon Phuah <shannonphuah2005@gmail.com>
2026-03-18 04:32:23 +0000

    Updated database.py

--- app/db/database.py
+++ app/db/database.py
@@ -1,12 +1,28 @@
 # app/db/database.py
 import asyncpg
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+pool = None  # global connection pool
+
+async def connect_to_db():
+    global pool
+    try:
+        pool = await asyncpg.create_pool(DATABASE_URL)
+        print("Database pool created")
+    except Exception as e:
+        print("Failed to connect to database:", e)
+        pool = None
+
+async def disconnect_db():
+    global pool
+    if pool:
+        await pool.close()
+        print("Database pool closed")
 
 async def get_db():
-    return await asyncpg.connect(DATABASE_URL)
+    if not pool:
+        raise Exception("Database pool not initialized")
+    async with pool.acquire() as connection:
+        yield connection
